87468617c12c9a7170080d8c68b8c21903cc9e15
Twan Hoogveld <twan.hoogveld@student.hu.nl>
2020-10-06 12:30:23 +0200

    Updated for multi platform & using sys args now

--- scraper.py
+++ scraper.py
@@ -1,21 +1,38 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
+import platform
+import sys
 
-username = "twanhoogveld"
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
-options.add_argument("disable-gpu")
-driver = webdriver.Chrome("/usr/local/bin/chromedriver",options=options)
-driver.get("https://www.instagram.com/{}/".format(username))
+if __name__ == '__main__':
+	#Options for a headless chrome webdriver.
+	options = webdriver.ChromeOptions()
+	options.add_argument('headless')
+	options.add_argument("disable-gpu")
 
-content = driver.page_source
-soup = BeautifulSoup(content,features="html.parser")
+	#Check platform.
+	if platform.system() == 'Windows':
+		#Webdriver for Windows
+		driver = webdriver.Chrome("C:\\chromedriver\\chromedriver.exe",options=options)
+	else:
+		#Webdriver for Linux
+		driver = webdriver.Chrome("/usr/local/bin/chromedriver",options=options)
 
-data = []
-for a in soup.findAll('a',href=True, attrs={'class':'-nal3'}):
-	data.append(a.find('span', attrs={'class':'g47SY'}).text)
+	#Get the webpage and username from the CLI
+	try:
+		username = str(sys.argv[1])
+	except IndexError:
+		exit()
+	driver.get("https://www.instagram.com/{}/".format(username))
 
-print("Posts : {}".format(data[0]))
-print("followers : {}".format(data[1]))
-print("following : {}".format(data[2]))
+	#Make it readable
+	content = driver.page_source
+	soup = BeautifulSoup(content,features="html.parser")
+
+	#Find the classes containing the data we want
+	data = []
+	for a in soup.findAll('a',href=True, attrs={'class':'-nal3'}):
+		data.append(a.find('span', attrs={'class':'g47SY'}).text)
+
+	#Print.
+	print("User : {} \nPosts : {} \nFollowers: {} \nFollowing {}".format(username,data[0],data[1],data[2]))
